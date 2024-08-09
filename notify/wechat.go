67a35da317166c247fcf34cfe0b26d737730d45f
Chuanjian Wang <me@ckeyer.com>
2024-08-09 17:24:27 +0800

    add wp & fix logs.Get

--- notify/wechat.go
+++ notify/wechat.go
@@ -2,6 +2,7 @@ package notify
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/silenceper/wechat/v2/officialaccount/message"
 	"github.com/ygpkg/yg-go/logs"
@@ -9,24 +10,25 @@ import (
 )
 
 // SendWechatOfficialAccountTemplateMsg 发送微信公众号模板消息
-func SendWechatOfficialAccountTemplateMsg(ctx context.Context, group, key, tplname string, msg *message.TemplateMessage) {
+func SendWechatOfficialAccountTemplateMsg(ctx context.Context, group, key, tplname string, msg *message.TemplateMessage) error {
 	cfg, mp, err := wechatmp.GetWechatOfficialAccount(group, key)
 	if err != nil {
 		logs.ErrorContextf(ctx, "SendWechatTemplateMsg: get wechat official account failed, %s", err)
-		return
+		return err
 	}
 
 	if msg.TemplateID == "" {
 		msg.TemplateID = cfg.Templates[tplname]
 		if msg.TemplateID == "" {
 			logs.ErrorContextf(ctx, "SendWechatTemplateMsg: template not found, tplname: %s", tplname)
-			return
+			return fmt.Errorf("template not found, tplname: %s", tplname)
 		}
 	}
 	msgid, err := mp.GetTemplate().Send(msg)
 	if err != nil {
 		logs.ErrorContextf(ctx, "SendWechatTemplateMsg: send template message failed, %s", err)
-		return
+		return err
 	}
 	logs.Infof("SendWechatTemplateMsg: send template message success, msgid: %v, body: %+v", msgid, msg)
+	return nil
 }
