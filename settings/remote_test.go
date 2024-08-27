464b1c2b778ceb422b0ae718156e748102f18a6a
Chuanjian Wang <me@ckeyer.com>
2024-08-27 22:19:31 +0800

    test aksk

--- settings/remote_test.go
+++ settings/remote_test.go
@@ -2,7 +2,6 @@ package settings
 
 import "testing"
 
-// "https://yygu.cn/v2/cook.GetSettingContent", "AK1yrh4XGgkTmYSom66rdx7mja5295Lo", "SKRJZOTJW5wm7wJkl0OnoB0a67RDEOrU", "roc.prod"
 func TestRemoteGet(t *testing.T) {
 	cli := NewRemoteSettingClientWithEnv()
 	content, err := cli.Get("account_main")
