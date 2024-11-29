721f5edd2c930209a81fdea9e169f4215ba916a0
skf <1306517867@qq.com>
2024-11-29 11:26:56 +0800

    fix

--- storage/minoss_test.go
+++ storage/minoss_test.go
@@ -1,23 +1,24 @@
 package storage
 
 import (
+	"bytes"
+	"context"
 	"fmt"
 	"io/ioutil"
+	"os"
 	"testing"
 
 	"github.com/ygpkg/yg-go/config"
 )
 
-var defaultCfg = config.MinossConfig{
-	EndPoint:        "out.yygu.cn:53014",
-	AccessKeyID:     "ygadmin",
-	SecretAccessKey: "tqh9kby01lwqe8ec",
-	Bucket:          "default-bucket",
-}
-
 // 连接Minoss
 func TestNewMinBucketClient(t *testing.T) {
-
+	var defaultCfg = config.MinossConfig{
+		EndPoint:        os.Getenv("END_POINT"),
+		AccessKeyID:     os.Getenv("ACCESS_KEY_ID"),
+		SecretAccessKey: os.Getenv("SECRET_ACCESS_KEY_ID"),
+		Bucket:          "default-bucket",
+	}
 	mc, err := NewMinFs(defaultCfg, config.StorageOption{})
 	if err != nil {
 		fmt.Println(err)
@@ -28,35 +29,50 @@ func TestNewMinBucketClient(t *testing.T) {
 	t.Logf("%+v", mc)
 }
 
-// func TestMinBucketClient_UploadFile(t *testing.T) {
+func TestMinBucketClient_UploadFile(t *testing.T) {
+	var defaultCfg = config.MinossConfig{
+		EndPoint:        os.Getenv("END_POINT"),
+		AccessKeyID:     os.Getenv("ACCESS_KEY_ID"),
+		SecretAccessKey: os.Getenv("SECRET_ACCESS_KEY_ID"),
+		Bucket:          "default-bucket",
+	}
+	mc, err := NewMinFs(defaultCfg, config.StorageOption{})
+	if err != nil {
+		t.Logf(err.Error())
+	}
+	content := []byte("this is a test")
+	path := "test/a/test.txt"
+	if err := mc.Save(context.Background(), &FileInfo{StoragePath: path, Size: int64(len(content))}, bytes.NewBuffer(content)); err != nil {
+		t.Logf(err.Error())
+	}
+}
 
-// 	mc, err := NewMinFs(defaultCfg, config.StorageOption{})
-// 	if err != nil {
-// 		t.Logf(err.Error())
-// 	}
-// 	content := []byte("this is a test")
-// 	path := "test/a/test.txt"
-// 	if err := mc.Save(context.Background(), &FileInfo{StoragePath: path, Size: int64(len(content))}, bytes.NewBuffer(content)); err != nil {
-// 		t.Logf(err.Error())
-// 	}
-// }
-
-// func TestMinBucketClient_GetPresignedURL(t *testing.T) {
-
-// 	mc, err := NewMinFs(defaultCfg, config.StorageOption{})
-// 	if err != nil {
-// 		t.Logf(err.Error())
-// 	}
-// 	path := "test/a/test.txt"
-// 	url, err := mc.GetPresignedURL(path)
-// 	if err != nil {
-// 		t.Logf(err.Error())
-// 	}
-// 	fmt.Println(url)
-// }
+func TestMinBucketClient_GetPresignedURL(t *testing.T) {
+	var defaultCfg = config.MinossConfig{
+		EndPoint:        os.Getenv("END_POINT"),
+		AccessKeyID:     os.Getenv("ACCESS_KEY_ID"),
+		SecretAccessKey: os.Getenv("SECRET_ACCESS_KEY_ID"),
+		Bucket:          "default-bucket",
+	}
+	mc, err := NewMinFs(defaultCfg, config.StorageOption{})
+	if err != nil {
+		t.Logf(err.Error())
+	}
+	path := "test/a/test.txt"
+	url, err := mc.GetPresignedURL(path)
+	if err != nil {
+		t.Logf(err.Error())
+	}
+	fmt.Println(url)
+}
 
 func TestMinBucketClient_ReadFile(t *testing.T) {
-
+	var defaultCfg = config.MinossConfig{
+		EndPoint:        os.Getenv("END_POINT"),
+		AccessKeyID:     os.Getenv("ACCESS_KEY_ID"),
+		SecretAccessKey: os.Getenv("SECRET_ACCESS_KEY_ID"),
+		Bucket:          "default-bucket",
+	}
 	mc, err := NewMinFs(defaultCfg, config.StorageOption{})
 	if err != nil {
 		t.Logf(err.Error())
@@ -71,7 +87,12 @@ func TestMinBucketClient_ReadFile(t *testing.T) {
 }
 
 func TestMinBucketClient_DeleteFile(t *testing.T) {
-
+	var defaultCfg = config.MinossConfig{
+		EndPoint:        os.Getenv("END_POINT"),
+		AccessKeyID:     os.Getenv("ACCESS_KEY_ID"),
+		SecretAccessKey: os.Getenv("SECRET_ACCESS_KEY_ID"),
+		Bucket:          "default-bucket",
+	}
 	mc, err := NewMinFs(defaultCfg, config.StorageOption{})
 	if err != nil {
 		t.Logf(err.Error())
