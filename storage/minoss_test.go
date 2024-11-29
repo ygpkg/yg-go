3dc8b69b3b0914488f76a17d66e0cc9d63e05ef5
skf <1306517867@qq.com>
2024-11-29 10:54:50 +0800

    minio test

--- storage/minoss_test.go
+++ storage/minoss_test.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"fmt"
+	"io/ioutil"
 	"testing"
 
 	"github.com/ygpkg/yg-go/config"
@@ -25,4 +26,59 @@ func TestNewMinBucketClient(t *testing.T) {
 
 	}
 	t.Logf("%+v", mc)
+}
+
+// func TestMinBucketClient_UploadFile(t *testing.T) {
+
+// 	mc, err := NewMinFs(defaultCfg, config.StorageOption{})
+// 	if err != nil {
+// 		t.Logf(err.Error())
+// 	}
+// 	content := []byte("this is a test")
+// 	path := "test/a/test.txt"
+// 	if err := mc.Save(context.Background(), &FileInfo{StoragePath: path, Size: int64(len(content))}, bytes.NewBuffer(content)); err != nil {
+// 		t.Logf(err.Error())
+// 	}
+// }
+
+// func TestMinBucketClient_GetPresignedURL(t *testing.T) {
+
+// 	mc, err := NewMinFs(defaultCfg, config.StorageOption{})
+// 	if err != nil {
+// 		t.Logf(err.Error())
+// 	}
+// 	path := "test/a/test.txt"
+// 	url, err := mc.GetPresignedURL(path)
+// 	if err != nil {
+// 		t.Logf(err.Error())
+// 	}
+// 	fmt.Println(url)
+// }
+
+func TestMinBucketClient_ReadFile(t *testing.T) {
+
+	mc, err := NewMinFs(defaultCfg, config.StorageOption{})
+	if err != nil {
+		t.Logf(err.Error())
+	}
+	path := "test/a/test.txt"
+	file, err := mc.ReadFile(path)
+	if err != nil {
+		t.Logf(err.Error())
+	}
+	data, _ := ioutil.ReadAll(file)
+	fmt.Println(string(data))
+}
+
+func TestMinBucketClient_DeleteFile(t *testing.T) {
+
+	mc, err := NewMinFs(defaultCfg, config.StorageOption{})
+	if err != nil {
+		t.Logf(err.Error())
+	}
+	path := "test/a/test.txt"
+	err = mc.DeleteFile(path)
+	if err != nil {
+		t.Logf(err.Error())
+	}
 }
