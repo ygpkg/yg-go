9f1cd00d928d8f32a0a2305c2a7d1d1b51c5fac5
morehao <morehao@qq.com>
2025-04-29 09:01:41 +0800

    --story=1002531 --user=宋浩 【开发】yg-go 封装 esutil 包 https://www.tapd.cn/36285394/s/1036839 (#20)
    * --story=1002531 --user=宋浩 【开发】yg-go 封装 esutil 包 https://www.tapd.cn/36285394/s/1036839
    esquery 构建 dsl
    
    * --story=1002531 --user=宋浩 【开发】yg-go 封装 esutil 包 https://www.tapd.cn/36285394/s/1036839
    
    esquery.Map 改为类型别名
    
    * --story=1002531 --user=宋浩 【开发】yg-go 封装 esutil 包 https://www.tapd.cn/36285394/s/1036839
    
    高亮 Option 实现调整
    
    * --story=1002531 --user=宋浩 【开发】yg-go 封装 esutil 包 https://www.tapd.cn/36285394/s/1036839
    
    BuildMap 函数入参命名调整
    
    * feat(esquery): build.Set 改为公有方法

---
+++ dbtools/esquery/util.go
@@ -0,0 +1,21 @@
+package esquery
+
+// BuildMap 用于构造map[string]interface{}的辅助方法
+// kvs 为连续的 key-value 对，key 必须是 string 类型，如 ["key", "value", "key2", "value2", ...]
+func BuildMap(kvs ...interface{}) Map {
+	m := make(map[string]interface{})
+	for i := 0; i < len(kvs); i += 2 {
+		if i+1 >= len(kvs) {
+			// 如果没有成对，跳过
+			break
+		}
+		key, ok := kvs[i].(string)
+		if !ok {
+			// 如果 key 不是 string 类型，则跳过这一对
+			continue
+		}
+		// 直接把合法的 key 和 value 加入 map
+		m[key] = kvs[i+1]
+	}
+	return m
+}
