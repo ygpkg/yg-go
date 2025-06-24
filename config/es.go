eb232975666c2dd079997e4427a05fd88e4eeb95
skf <1306517867@qq.com>
2025-06-24 12:15:47 +0800

    fix esconfig

--- config/es.go
+++ config/es.go
@@ -4,9 +4,9 @@ import "time"
 
 // ESConfig ES配置
 type ESConfig struct {
-	Addresses     []string
-	Username      string
-	Password      string
-	MaxRetries    int           // 最大重试次数
-	SlowThreshold time.Duration // 慢查询阈值，示例 100ms
+	Addresses     []string      `yaml:"addresses"`
+	Username      string        `yaml:"username"`
+	Password      string        `yaml:"password"`
+	MaxRetries    int           `yaml:"max_retries"`    // 最大重试次数
+	SlowThreshold time.Duration `yaml:"slow_threshold"` // 慢查询阈值，示例 100ms
 }
